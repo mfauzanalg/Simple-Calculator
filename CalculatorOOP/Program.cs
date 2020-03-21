642d796557e199b1fde3a6736ce88f8b35b5d483
mfauzanalg <mfauzanalg@gmail.com>
2020-03-21 08:57:51 +0700

    yaAllah cape ayo guys kerjain

--- CalculatorOOP/Program.cs
+++ CalculatorOOP/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using CalculatorOOP;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace CButton
+{
+    public abstract partial class CalcButton : System.Windows.Forms.Button
+    {
+        public Button B;
+        public abstract void OnClick(object sender, RoutedEventArgs e);
+    }
+    public class MyButton : Button
+    {
+        public MyButton() : base()
+        {
+            // set whatever styling properties needed here
+            this.ForeColor = System.Drawing.Color.Red; ;
+        }
+    }
+}
